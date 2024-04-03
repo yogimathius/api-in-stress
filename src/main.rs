3d638a34e50e13603e7b7167701a6852d9c2df6d
Mathius <info@yogimathius.dev>
2024-04-02 21:49:06 -0700

    feat: add axum

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-async fn hello() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+async fn hello() -> &'static str {
+    "Hello, world!"
 }
 
 #[tokio::main]
 async fn main() {
-    hello().await;
+    let app = Router::new()
+        .route("/hello", get(hello));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
