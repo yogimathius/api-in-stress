119a691feb98de1f3d2a797d96c83a5c2d801707
Mathius <info@yogimathius.dev>
2024-04-05 21:32:40 -0700

    refactor: move handlers to handlers.rs

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,16 @@
 use axum::{
-    error_handling::HandleErrorLayer, extract::{Json, Path, Query}, http::StatusCode, response::IntoResponse, routing::{get, post}, BoxError, Extension, Router
+    error_handling::HandleErrorLayer,
+    routing::{get, post},
+    Extension,
+    Router
 };
 use tower::{timeout::TimeoutLayer, ServiceBuilder};
 
-use std::collections::HashMap;
 use std::time::Duration;
 
 mod storage;
-
-use storage::{Storage, Warrior};
-
-async fn create_warrior(storage: Extension<Storage>, Json(payload): Json<Warrior>) -> impl IntoResponse {
-    println!("Creating warrior: {:?}", payload);
-    // TODO - Error handling
-    (StatusCode::OK, [("x-foo", "bar")], Json(storage.create_warrior(payload).await))
-}
-
-async fn get_warrior(Path(user_id): Path<u32>, storage: Extension<Storage>) -> impl IntoResponse {
-    println!("Warrior fetched for id: {:?}", user_id);
-
-    // TODO - Error handling
-    (StatusCode::OK, [("x-foo", "bar")], Json(storage.get_warrior(user_id.to_string()).await))
-}
-
-async fn search_warriors(Query(params): Query<HashMap<String, String>>, storage: Extension<Storage>) -> impl IntoResponse {
-    println!("Warriors searched for: {:?}", params.get("term").unwrap_or(&"".to_string()));
-    // TODO - Error handling
-    // TODO - Implement search logic
-    (StatusCode::OK, [("x-foo", "bar")], Json(storage.search_warriors("".to_string()).await))
-}
-
-async fn count_warriors(storage: Extension<Storage>) -> impl IntoResponse {
-    println!("Warriors counted");
-    // TODO - Error handling
-    let warrior_count = Json(storage.count_warriors().await);
-
-    (StatusCode::OK, [("x-foo", "bar")], warrior_count)
-}
-
-async fn handle_timeout_error(err: BoxError) -> (StatusCode, String) {
-    println!("Error: {:?}", err);
-    if err.is::<tower::timeout::error::Elapsed>() {
-        (
-            StatusCode::REQUEST_TIMEOUT,
-            "Request took too long".to_string(),
-        )
-    } else {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Unhandled internal error: {err}"),
-        )
-    }
-}
+mod handlers;
+use handlers::{create_warrior, get_warrior, search_warriors, count_warriors, handle_timeout_error};
 
 #[tokio::main]
 async fn main() {
