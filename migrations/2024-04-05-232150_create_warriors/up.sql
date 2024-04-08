e5a9a3f7d06692fb96763c2f36571bffad2b8369
Mathius <info@yogimathius.dev>
2024-04-08 01:52:10 -0700

    chore: remove diesel

--- migrations/2024-04-05-232150_create_warriors/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-CREATE TABLE warriors (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR NOT NULL,
-  dob VARCHAR NOT NULL
-)
--- src/schema.rs
+++
@@ -1,9 +0,0 @@
-// @generated automatically by Diesel CLI.
-
-diesel::table! {
-    warriors (id) {
-        id -> Int4,
-        name -> Varchar,
-        dob -> Varchar,
-    }
-}
