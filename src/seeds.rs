00488d107b902fa07de631ca2513ab23f32e8faf
Mathius <info@yogimathius.dev>
2024-04-08 23:03:46 -0700

    fix: insert seeds successfully

--- src/seeds.rs
+++ src/seeds.rs
@@ -6,7 +6,7 @@ pub async fn run_seeds(
     pool: sqlx::Pool<Postgres>
 ) {
 
-    let mut initial_warriors = Vec::new();
+    let mut initial_warriors: Vec<NewWarrior> = Vec::new();
 
     for i in 1..=75 {
         let warrior = NewWarrior {
@@ -15,22 +15,22 @@ pub async fn run_seeds(
             // fight_skills: vec![format!("Skill {}", i * 2), format!("Skill {}", i * 2 + 1)],
         };
 
-        initial_warriors.push((warrior.name.clone(), warrior.dob.clone()));
+        initial_warriors.push((warrior));
     }
 
     let mut conn = pool.acquire().await.expect("Error acquiring connection from pool");
     for warrior in initial_warriors {
         println!("Inserting warrior: {:?}", warrior);
+
         let mut query_builder: QueryBuilder<Postgres> = QueryBuilder::new(
-            "INSERT INTO warriors (name, dob) VALUES ("
+            r#"INSERT INTO warriors (name, dob) VALUES ('"#
         );
-
-        query_builder.push(warrior.0);
-        query_builder.push(", ");
-        query_builder.push(warrior.1);
-        query_builder.push(") RETURNING id, name, dob");
+    
+        query_builder.push(warrior.name);
+        query_builder.push(r#"', '"#);
+        query_builder.push(warrior.dob);
+        query_builder.push(r#"') RETURNING id, name, dob;"#);
         
-
         sqlx::query(query_builder.build().sql())
         .fetch_one(&mut conn)
         .await
