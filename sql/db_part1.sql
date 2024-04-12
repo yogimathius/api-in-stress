da14a3959de68ec2f21151972972a894cc98ca2e
Mathius <info@yogimathius.dev>
2024-04-11 22:21:45 -0700

    chore: rename sql tables

--- sql/db_part1.sql
+++ sql/db_part1.sql
@@ -1,18 +1,18 @@
-CREATE TABLE IF NOT EXISTS warriors_part1 (
+CREATE TABLE IF NOT EXISTS warriors_1 (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
   dob VARCHAR NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS skills_part1 (
+CREATE TABLE IF NOT EXISTS skills_1 (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS warrior_skills_part1 (
+CREATE TABLE IF NOT EXISTS warrior_skills_1 (
   id SERIAL PRIMARY KEY,
   warrior_id INT NOT NULL,
   skill_id INT NOT NULL,
-  FOREIGN KEY (warrior_id) REFERENCES warriors_part1(id),
-  FOREIGN KEY (skill_id) REFERENCES skills_part1(id)
+  FOREIGN KEY (warrior_id) REFERENCES warriors_1(id),
+  FOREIGN KEY (skill_id) REFERENCES skills_1(id)
 );
--- sql/db_part2.sql
+++ sql/db_part2.sql
@@ -1,18 +1,18 @@
-CREATE TABLE IF NOT EXISTS warriors_part2 (
+CREATE TABLE IF NOT EXISTS warriors_2 (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
   dob VARCHAR NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS skills_part2 (
+CREATE TABLE IF NOT EXISTS skills_2 (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS warrior_skills_part2 (
+CREATE TABLE IF NOT EXISTS warrior_skills_s2 (
   id SERIAL PRIMARY KEY,
   warrior_id INT NOT NULL,
   skill_id INT NOT NULL,
-  FOREIGN KEY (warrior_id) REFERENCES warriors_part2(id),
-  FOREIGN KEY (skill_id) REFERENCES skills_part2(id)
+  FOREIGN KEY (warrior_id) REFERENCES warriors_2(id),
+  FOREIGN KEY (skill_id) REFERENCES skills_2(id)
 );
