e74d1eb35699a568cc3a88f819f2ffb349765824
Mathius <info@yogimathius.dev>
2024-04-14 00:32:29 -0700

    feat: hook up redis to state

--- src/app.rs
+++ src/app.rs
@@ -5,20 +5,25 @@ use axum::{
 };
 use std::time::Duration;
 use tower::{timeout::TimeoutLayer, ServiceBuilder};
-use crate::handlers::{create_warrior, get_warrior, search_warriors, count_warriors, handle_timeout_error};
+use crate::handlers::{create_warrior, get_warrior, search_warriors, count_warriors, handle_timeout_error, AppState};
 use crate::database::create_pool;
 use crate::redis;
 
 pub async fn create_app() -> Router {
     let pool = create_pool().await;
     let redis_pool = redis::create_pool().await;
+
+    let app_state = AppState {
+        db_store: pool,
+        redis_store: redis_pool,
+    };
+
     Router::new()
         .route("/warrior", post(create_warrior) )
         .route("/warrior/:id", get(get_warrior))
         .route("/warrior", get(search_warriors))
         .route("/counting-warriors", get(count_warriors))
-        .with_state(pool)
-        .with_state(redis_pool)
+        .with_state(app_state)
         .layer(tower::ServiceBuilder::new().concurrency_limit(64))
         .layer(
             ServiceBuilder::new()
