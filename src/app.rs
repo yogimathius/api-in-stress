9fc1a7eecad4f4b6787f811475985bb7f65f468b
Mathius <info@yogimathius.dev>
2024-04-13 23:33:22 -0700

    feat: add redis to app state

--- src/app.rs
+++ src/app.rs
@@ -7,15 +7,18 @@ use std::time::Duration;
 use tower::{timeout::TimeoutLayer, ServiceBuilder};
 use crate::handlers::{create_warrior, get_warrior, search_warriors, count_warriors, handle_timeout_error};
 use crate::database::create_pool;
+use crate::redis;
 
 pub async fn create_app() -> Router {
     let pool = create_pool().await;
+    let redis_pool = redis::create_pool().await;
     Router::new()
         .route("/warrior", post(create_warrior) )
         .route("/warrior/:id", get(get_warrior))
         .route("/warrior", get(search_warriors))
         .route("/counting-warriors", get(count_warriors))
         .with_state(pool)
+        .with_state(redis_pool)
         .layer(tower::ServiceBuilder::new().concurrency_limit(64))
         .layer(
             ServiceBuilder::new()
