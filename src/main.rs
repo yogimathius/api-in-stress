70d4000b525d8996733b067edccc77981fcc2744
Mathius <info@yogimathius.dev>
2024-04-13 22:23:24 -0700

    chore: DRY up main

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,12 @@ use axum::{
 
 };
 use tower::{timeout::TimeoutLayer, ServiceBuilder};
-use std::{sync::Arc, time::Duration};
+use std::time::Duration;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use hyper::body::Incoming;
 use hyper_util::rt::{TokioExecutor, TokioIo};
 use hyper_util::server;
 use tower::Service;
-// use tokio::sync::Semaphore;
 
 use database::{create_warrior, get_warrior, search_warriors, count_warriors, handle_timeout_error};
 
@@ -35,11 +34,8 @@ async fn main() {
     dotenv().ok();
 
     println!("DATABASE_URL: {:?}", std::env::var("DATABASE_URL"));
-    // let db_url = std::env::var("DATABASE_URL").unwrap();
-    // let mut conn = PgConnection::establish(&db_url).unwrap();
     let db_connection_str = std::env::var("DATABASE_URL")
         .unwrap_or_else(|_| "postgres://postgres:password@localhost".to_string());
-    // let semaphore = Arc::new(Semaphore::new(10));
 
     // set up connection pool
     let pool = PgPoolOptions::new()
@@ -66,39 +62,20 @@ async fn main() {
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on: {}", listener.local_addr().unwrap());
-    // Continuously accept new connections.
+
     loop {
-        // In this example we discard the remote address. See `fn serve_with_connect_info` for how
-        // to expose that.
         let (socket, _remote_addr) = listener.accept().await.unwrap();
 
-        // We don't need to call `poll_ready` because `Router` is always ready.
         let tower_service = app.clone();
 
-        // Spawn a task to handle the connection. That way we can multiple connections
-        // concurrently.
         tokio::spawn(async move {
-            // Hyper has its own `AsyncRead` and `AsyncWrite` traits and doesn't use tokio.
-            // `TokioIo` converts between them.
             let socket = TokioIo::new(socket);
 
-            // Hyper also has its own `Service` trait and doesn't use tower. We can use
-            // `hyper::service::service_fn` to create a hyper `Service` that calls our app through
-            // `tower::Service::call`.
             let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
-                // We have to clone `tower_service` because hyper's `Service` uses `&self` whereas
-                // tower's `Service` requires `&mut self`.
-                //
-                // We don't need to call `poll_ready` since `Router` is always ready.
                 tower_service.clone().call(request)
             });
 
-            // `server::conn::auto::Builder` supports both http1 and http2.
-            //
-            // `TokioExecutor` tells hyper to use `tokio::spawn` to spawn tasks.
             if let Err(err) = server::conn::auto::Builder::new(TokioExecutor::new())
-                // `serve_connection_with_upgrades` is required for websockets. If you don't need
-                // that you can use `serve_connection` instead.
                 .serve_connection(socket, hyper_service)
                 .await
             {
