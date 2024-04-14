35942394a458944f5db841938b412a31f05328b0
Mathius <info@yogimathius.dev>
2024-04-13 23:29:10 -0700

    refactor: create serve and handle fns in main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::extract::Request;
+use tokio::net::TcpListener;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use hyper::body::Incoming;
 use hyper_util::rt::{TokioExecutor, TokioIo};
@@ -9,6 +10,31 @@ mod database;
 mod handlers;
 mod models;
 mod app;
+
+async fn handle_connection(socket: tokio::net::TcpStream, app: axum::Router) {
+    let socket = TokioIo::new(socket);
+    let tower_service = app.clone();
+
+    let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
+        tower_service.clone().call(request)
+    });
+
+    if let Err(err) = server::conn::auto::Builder::new(TokioExecutor::new())
+    .serve_connection(socket, hyper_service)
+        .await
+    {
+        eprintln!("failed to serve connection: {}", err);
+    }
+}
+
+async fn serve(listener: TcpListener, app: axum::Router) {
+    println!("Listening on: {}", listener.local_addr().unwrap());
+
+    loop {
+        let (socket, _remote_addr) = listener.accept().await.unwrap();
+        tokio::spawn(handle_connection(socket, app.clone()));
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -24,24 +50,5 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on: {}", listener.local_addr().unwrap());
 
-    loop {
-        let (socket, _remote_addr) = listener.accept().await.unwrap();
-
-        let tower_service = app.clone();
-
-        tokio::spawn(async move {
-            let socket = TokioIo::new(socket);
-
-            let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
-                tower_service.clone().call(request)
-            });
-
-            if let Err(err) = server::conn::auto::Builder::new(TokioExecutor::new())
-                .serve_connection(socket, hyper_service)
-                .await
-            {
-                eprintln!("failed to serve connection: {}", err);
-            }
-        });
-    }
+    serve(listener, app).await;
 }
