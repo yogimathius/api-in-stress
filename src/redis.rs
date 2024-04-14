31e4f7cc8bfa94917dad736ea1e47b3a52580ce6
Mathius <info@yogimathius.dev>
2024-04-13 23:33:12 -0700

    chore: add redis mod

---
+++ src/redis.rs
@@ -0,0 +1,19 @@
+use bb8_redis::RedisConnectionManager;
+use redis::AsyncCommands;
+
+use bb8_redis::bb8;
+
+pub async fn create_pool() -> bb8::Pool<RedisConnectionManager> {
+    let manager = RedisConnectionManager::new("redis://localhost").unwrap();
+    let pool = bb8::Pool::builder().build(manager).await.unwrap();
+
+    {
+        // ping the database before starting
+        let mut conn = pool.get().await.unwrap();
+        conn.set::<&str, &str, ()>("foo", "bar").await.unwrap();
+        let result: String = conn.get("foo").await.unwrap();
+        assert_eq!(result, "bar");
+    }
+    tracing::debug!("successfully connected to redis and pinged it");
+    pool
+}
