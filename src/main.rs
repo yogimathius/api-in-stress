3e564e66393765486899ee541ec289ff37ca5319
Mathius <info@yogimathius.dev>
2024-04-13 22:37:48 -0700

    refactor: use databse in main, cleanup main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use sqlx::postgres::PgPoolOptions;
-mod handlers;
-mod models;
-
 use axum::{
     error_handling::HandleErrorLayer,
     routing::{get, post},
     Router,
     extract::Request
-
 };
 use tower::{timeout::TimeoutLayer, ServiceBuilder};
 use std::time::Duration;
@@ -19,7 +14,9 @@ use tower::Service;
 
 use handlers::{create_warrior, get_warrior, search_warriors, count_warriors, handle_timeout_error};
 
-use dotenvy::dotenv;
+mod database;
+mod handlers;
+mod models;
 
 #[tokio::main]
 async fn main() {
@@ -31,19 +28,7 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    dotenv().ok();
-
-    let db_connection_str = std::env::var("DATABASE_URL")
-    .unwrap_or_else(|_| "postgres://postgres:password@localhost".to_string());
-
-    println!("db_connection_str: {}", db_connection_str);
-
-    let pool = PgPoolOptions::new()
-        .max_connections(60000)
-        .acquire_timeout(Duration::from_secs(3))
-        .connect(&db_connection_str)
-        .await
-        .expect("can't connect to database");
+    let pool = database::create_pool().await;
 
     let app = Router::new()
         .route("/warrior", post(create_warrior) )
@@ -59,7 +44,6 @@ async fn main() {
                 .layer(HandleErrorLayer::new(handle_timeout_error))
                 .layer(TimeoutLayer::new(Duration::from_secs(30)))
         );
-
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on: {}", listener.local_addr().unwrap());
 
@@ -79,7 +63,8 @@ async fn main() {
                 .serve_connection(socket, hyper_service)
                 .await
             {
-                eprintln!("failed to serve connection: {err:#}");
+                eprintln!("failed to serve connection: {}", err);
             }
         });
-    }}
+    }
+}
