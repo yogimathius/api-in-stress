5a47771ea2cf71085c85f3ea5d72fe2ca87d3d42
Mathius <info@yogimathius.dev>
2024-04-13 23:33:40 -0700

    refactor: move create_warrior in handlers

--- src/handlers.rs
+++ src/handlers.rs
@@ -10,6 +10,25 @@ use crate::models::{Warrior, NewWarrior};
 use std::collections::HashMap;
 use tower::BoxError;
 
+pub struct DatabaseConnection(pub sqlx::pool::PoolConnection<sqlx::Postgres>);
+
+#[async_trait]
+impl<S> FromRequestParts<S> for DatabaseConnection
+where
+    PgPool: FromRef<S>,
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let pool = PgPool::from_ref(state);
+
+        let conn = pool.acquire().await.map_err(internal_error)?;
+
+        Ok(Self(conn))
+    }
+}
+
 pub async fn create_warrior(
     DatabaseConnection(mut conn): DatabaseConnection,
     Json(warrior): Json<NewWarrior>
@@ -38,25 +57,6 @@ pub async fn create_warrior(
     };
 
     Ok(Json(warrior))
-}
-
-pub struct DatabaseConnection(pub sqlx::pool::PoolConnection<sqlx::Postgres>);
-
-#[async_trait]
-impl<S> FromRequestParts<S> for DatabaseConnection
-where
-    PgPool: FromRef<S>,
-    S: Send + Sync,
-{
-    type Rejection = (StatusCode, String);
-
-    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let pool = PgPool::from_ref(state);
-
-        let conn = pool.acquire().await.map_err(internal_error)?;
-
-        Ok(Self(conn))
-    }
 }
 
 pub async fn get_warrior(
