1e7419880330ae0d263c724ac9e29a12574823d5
Mathius <info@yogimathius.dev>
2024-04-14 00:50:26 -0700

    fix: use docker server for redis url

--- src/redis.rs
+++ src/redis.rs
@@ -4,7 +4,8 @@ use redis::AsyncCommands;
 use bb8_redis::bb8;
 
 pub async fn create_pool() -> bb8::Pool<RedisConnectionManager> {
-    let manager = RedisConnectionManager::new("redis://localhost").unwrap();
+    let manager = RedisConnectionManager::new("redis://redis:6379").unwrap();
+    println!("created redis connection manager {:?}", manager);
     let pool = bb8::Pool::builder().build(manager).await.unwrap();
 
     {
