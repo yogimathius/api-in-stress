e3652f1556e5a017962322770b9123d6604714a2
Mathius <info@yogimathius.dev>
2024-04-16 23:44:19 -0700

    chore: add skills to NewWarrior struct
    Co-authored-by: Tony Bronzo <thetone>

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,11 @@
-use sqlx::{FromRow, Encode};
+use sqlx::{postgres::PgHasArrayType, Encode, FromRow};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, serde::Deserialize, Encode)]
+#[derive(Debug, serde::Deserialize)]
 pub struct NewWarrior {
     pub name: String,
     pub dob: String,
+    pub skills: Vec<WarriorSkill>
 }
 
 #[derive(Debug, Deserialize, Serialize, FromRow)]
@@ -12,4 +13,9 @@ pub struct Warrior {
     pub id: String,
     pub name: String,
     pub dob: String,
+}
+
+#[derive(Debug, serde::Deserialize, Encode)]
+pub struct WarriorSkill {
+    pub name: String
 }
