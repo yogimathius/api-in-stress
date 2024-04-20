39c039c90ea4246c71ae49e2ba550ce50f46f4e3
Mathius <info@yogimathius.dev>
2024-04-19 21:46:30 -0700

    chore: add app_state.rs

---
+++ src/app_state.rs
@@ -0,0 +1,47 @@
+use bb8_redis::RedisConnectionManager;
+use bb8::Pool;
+use sqlx::postgres::PgPool;
+use axum::{
+    async_trait, extract::{FromRef, FromRequestParts}, http::{request::Parts, StatusCode}
+};
+use crate::handlers::internal_error;
+pub struct DatabaseConnection(pub sqlx::pool::PoolConnection<sqlx::Postgres>);
+type RedisPool = Pool<RedisConnectionManager>;
+pub struct RedisPoolWrapper(pub RedisPool);
+
+#[derive(Clone)]
+pub struct AppState {
+    pub(crate) db_store: sqlx::PgPool,
+    pub(crate) redis_store:  Pool<RedisConnectionManager>,
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for RedisPoolWrapper
+where
+    RedisPool: FromRef<S>,
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let redis_pool = RedisPool::from_ref(state);
+        Ok(Self(redis_pool))
+    }
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for DatabaseConnection
+where
+    PgPool: FromRef<S>,
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let pool = PgPool::from_ref(state);
+
+        let conn = pool.acquire().await.map_err(internal_error)?;
+
+        Ok(Self(conn))
+    }
+}
