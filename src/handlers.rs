812731e8646036e4c05600b4be1f4a902e97c7dd
Mathius <info@yogimathius.dev>
2024-04-19 21:46:48 -0700

    refactor: use app_state in handlers and app

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,9 +1,8 @@
+use crate::app_state::AppState;
 use serde_json;
-use bb8_redis::RedisConnectionManager;
-use bb8::Pool;
-use sqlx::{postgres::PgPool, Execute, Postgres, QueryBuilder, Row};
+use sqlx::Row;
 use axum::{
-    async_trait, extract::{FromRef, FromRequestParts, Path, Query, State}, http::{request::Parts, HeaderMap, StatusCode}, response::IntoResponse, Json
+    extract::{Path, Query, State}, http::{HeaderMap, StatusCode}, response::IntoResponse, Json
 };
 use redis::AsyncCommands;
 use std::time::SystemTime;
@@ -11,47 +10,6 @@ use std::time::SystemTime;
 use crate::models::{NewWarrior, Warrior};
 use std::collections::HashMap;
 use tower::BoxError;
-
-pub struct DatabaseConnection(pub sqlx::pool::PoolConnection<sqlx::Postgres>);
-type RedisPool = Pool<RedisConnectionManager>;
-pub struct RedisPoolWrapper(pub RedisPool);
-
-#[derive(Clone)]
-pub struct AppState {
-    pub(crate) db_store: sqlx::PgPool,
-    pub(crate) redis_store:  Pool<RedisConnectionManager>,
-}
-
-#[async_trait]
-impl<S> FromRequestParts<S> for RedisPoolWrapper
-where
-    RedisPool: FromRef<S>,
-    S: Send + Sync,
-{
-    type Rejection = (StatusCode, String);
-
-    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let redis_pool = RedisPool::from_ref(state);
-        Ok(Self(redis_pool))
-    }
-}
-
-#[async_trait]
-impl<S> FromRequestParts<S> for DatabaseConnection
-where
-    PgPool: FromRef<S>,
-    S: Send + Sync,
-{
-    type Rejection = (StatusCode, String);
-
-    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let pool = PgPool::from_ref(state);
-
-        let conn = pool.acquire().await.map_err(internal_error)?;
-
-        Ok(Self(conn))
-    }
-}
 
 pub async fn create_warrior(
     State(state): State<AppState>,
