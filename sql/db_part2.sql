68e8b4079a4b5ef3de4dffa9a0f2cfef10a5c363
Mathius <info@yogimathius.dev>
2024-04-21 23:52:47 -0700

    chore: add indexes to db's

--- sql/db_part2.sql
+++ sql/db_part2.sql
@@ -16,3 +16,7 @@ CREATE TABLE IF NOT EXISTS warrior_skills_s2 (
   FOREIGN KEY (warrior_id) REFERENCES warriors_2(id),
   FOREIGN KEY (skill_id) REFERENCES skills_2(id)
 );
+
+CREATE INDEX idx_skills_name ON skills_2 (name);
+CREATE INDEX idx_warrior_skills_warrior_id ON warrior_skills_s2 (warrior_id);
+CREATE INDEX idx_warrior_skills_skill_id ON warrior_skills_s2 (skill_id);
