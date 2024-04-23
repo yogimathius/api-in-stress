e89f03bfec7480aa31b6b17abc057190718fde44
Mathius <info@yogimathius.dev>
2024-04-22 23:13:58 -0700

    cleanup: remove CREATE_WARRIOR query from queries

--- src/queries.rs
+++ src/queries.rs
@@ -1,19 +1,3 @@
-pub const CREATE_WARRIOR: &str = r#"
-WITH inserted_warrior AS (
-    INSERT INTO warriors (name, dob)
-    VALUES ($1, $2)
-    RETURNING id
-),
-inserted_warrior_skills AS (
-    INSERT INTO warrior_skills (warrior_id, skill_id)
-    SELECT inserted_warrior.id, s.id
-    FROM inserted_warrior
-    CROSS JOIN unnest($3::text[]) AS skill_name
-    JOIN skills s ON s.name = skill_name
-)
-SELECT id from inserted_warrior;
-"#;
-
 pub const GET_WARRIOR: &str = r#"
 SELECT warriors.*,
     (SELECT array_agg(skills.name)
