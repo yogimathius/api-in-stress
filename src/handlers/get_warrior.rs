02fe35562b613dbcb177cd8b409ff9e17f200ec5
Mathius <info@yogimathius.dev>
2024-04-22 23:13:46 -0700

    chore: update id type in get_warrior to string

--- src/handlers/get_warrior.rs
+++ src/handlers/get_warrior.rs
@@ -11,12 +11,12 @@ use crate::utilities::internal_error;
 
 pub async fn get_warrior(
     State(state): State<AppState>,
-    Path(user_id): Path<i32>,
+    Path(user_id): Path<String>,
 ) -> Result<Json<Warrior>, (StatusCode, String)> {
-
+    let user_id_clone = user_id.clone();
     let mut redis_conn: bb8::PooledConnection<'_, bb8_redis::RedisConnectionManager> = state.redis_store.get().await.unwrap();
 
-    if let Ok(user_id) = redis_conn.get::<_, String>(&user_id).await {
+    if let Ok(user_id) = redis_conn.get::<_, String>(&user_id.clone()).await {
         let warrior: Warrior = serde_json::from_str(&user_id).unwrap();
         // report_time(start, "get_warrior from cache");
 
@@ -30,11 +30,11 @@ pub async fn get_warrior(
         .map_err(|err| internal_error(err))?;
 
     let warrior_json: String = serde_json::to_string(&warrior).unwrap();
-    let _ = redis_conn.set::<_, String, ()>(&user_id, warrior_json).await.map_err(|err: redis::RedisError| {
+    let _ = redis_conn.set::<_, String, ()>(user_id_clone, warrior_json).await.map_err(|err: redis::RedisError| {
         eprintln!("Failed to cache warrior: {:?}", err);
         StatusCode::INTERNAL_SERVER_ERROR
     });
-    println!("Warrior cached successfully");
+    // println!("Warrior cached successfully");
 
     Ok(Json(warrior))
 }
