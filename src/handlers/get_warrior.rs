72ace5a7c02f16e511d730dec5f7e37e4255cbe2
Mathius <info@yogimathius.dev>
2024-04-23 21:38:06 -0700

    fix: update responses in get_warrior

--- src/handlers/get_warrior.rs
+++ src/handlers/get_warrior.rs
@@ -1,7 +1,7 @@
 use crate::app_state::AppState;
 use serde_json;
 use axum::{
-    extract::{Path, State}, http::StatusCode, Json
+    extract::{Path, State}, http::{HeaderMap, StatusCode}, response::IntoResponse, Json
 };
 use redis::AsyncCommands;
 
@@ -14,21 +14,25 @@ use super::queries::GET_WARRIOR;
 pub async fn get_warrior(
     State(state): State<AppState>,
     Path(user_id): Path<String>,
-) -> Result<Json<Warrior>, (StatusCode, String)> {
+) -> impl IntoResponse {
     let user_id_clone = user_id.clone();
     let mut redis_conn: bb8::PooledConnection<'_, bb8_redis::RedisConnectionManager> = state.redis_store.get().await.unwrap();
-
+    let mut headers = HeaderMap::new();
+    headers.insert("content-type", "application/json".parse().unwrap());
+    
     if let Ok(user_id) = redis_conn.get::<_, String>(&user_id.clone()).await {
         let warrior: Warrior = serde_json::from_str(&user_id).unwrap();
 
-        return Ok(Json(warrior));
+        return (StatusCode::OK, headers, Json(warrior));
+
     }        
 
     let result: Option<Warrior> = sqlx::query_as(&GET_WARRIOR)
         .bind(user_id)
         .fetch_optional(&state.primary_db_store)
         .await
-        .map_err(|err| internal_error(err))?;
+        .map_err(|err| internal_error(err))
+        .unwrap();
 
 
     match result {
@@ -38,8 +42,16 @@ pub async fn get_warrior(
                 eprintln!("Failed to cache warrior: {:?}", err);
                 StatusCode::INTERNAL_SERVER_ERROR
             });
-            Ok(Json(warrior))
+            return (StatusCode::OK, headers, Json(warrior));
         },
-        None => Err((StatusCode::NOT_FOUND, "Warrior not found".to_string()))
+        None => {
+            let no_warrior: Warrior = Warrior {
+                id: "".to_string(),
+                name: "".to_string(),
+                dob: "".to_string(),
+                fight_skills: None
+            };
+            return (StatusCode::NOT_FOUND, headers, Json(no_warrior));
+        }
     }
 }
