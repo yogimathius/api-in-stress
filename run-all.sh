d260ce3d88f492757c87ef4eee6bb882ff5796e9
Mathius <info@yogimathius.dev>
2024-04-24 23:54:32 -0700

    fix: rename app in dockerfile to match cargo

--- run-all.sh
+++ run-all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Step 1: Build Database Docker Image and Start Replicas
-docker-compose -f docker-compose-db-replicas.yml up -d
+docker-compose -f docker-compose-db.yml up -d
 
 # Step 2: Build Redis Docker Image and Start Redis
 docker-compose -f docker-compose-redis.yml up -d
 
 # Step 3: Build API Server Docker Image
-docker-compose build
+# docker-compose build
 
-# Step 4: Start API Server
-docker-compose -f docker-compose.yml up
+# # Step 4: Start API Server
+# docker-compose -f docker-compose.yml up
