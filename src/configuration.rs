5c7e7e69f956684b09b1e875ca9ad9fe0928e4d5
Mathius <info@yogimathius.dev>
2024-04-24 21:23:14 -0700

    chore: add configuration.rs

--- src/configuration.rs
+++ src/configuration.rs
@@ -51,7 +51,6 @@ impl DatabaseSettings {
             .username(&self.username)
             .password(self.password.expose_secret())
             .port(self.port)
-            .ssl_mode(ssl_mode)
     }
 }
 
