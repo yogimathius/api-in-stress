10b251bdb37ee5588accdd31b868584cd9e38a02
Mathius <info@yogimathius.dev>
2024-04-27 23:01:07 -0700

    optimize: conditionally search for warriors with skills if search param is a skill

--- src/handlers/search_warriors.rs
+++ src/handlers/search_warriors.rs
@@ -1,16 +1,19 @@
+use super::queries::{SEARCH_WARRIORS, SEARCH_WARRIORS_WITH_SKILLS};
 use crate::app_state::AppState;
-use serde_json;
-use axum::{
-    extract::{Query, State}, http::{HeaderMap, StatusCode}, response::IntoResponse, Json
-};
 use crate::models::Warrior;
-use super::queries::SEARCH_WARRIORS;
 use crate::utilities::internal_error;
+use axum::{
+    extract::{Query, State},
+    http::{HeaderMap, StatusCode},
+    response::IntoResponse,
+    Json,
+};
+use serde_json;
 use std::collections::HashMap;
 
 pub async fn search_warriors(
     State(state): State<AppState>,
-    Query(params): Query<HashMap<String, String>>
+    Query(params): Query<HashMap<String, String>>,
 ) -> impl IntoResponse {
     let query_key = format!("warriors:{:?}", params.get("t"));
     let mut headers = HeaderMap::new();
@@ -20,14 +23,24 @@ pub async fn search_warriors(
         return (StatusCode::BAD_REQUEST, headers, Json(no_warriors));
     }
 
-    let warriors_json = state.redis_store.get(&query_key).await.unwrap_or("".to_string());
-    
+    let warriors_json = state
+        .redis_store
+        .get(&query_key)
+        .await
+        .unwrap_or("".to_string());
+
     if !warriors_json.is_empty() {
         let warriors: Vec<Warrior> = serde_json::from_str(&warriors_json).unwrap();
 
         return (StatusCode::OK, headers, Json(warriors));
     } else {
-        let warriors: Vec<Warrior> = sqlx::query_as(SEARCH_WARRIORS)
+        let skills_to_check = vec![params.get("t").unwrap().to_string()];
+        let query = if state.valid_skills.are_valid_skills(&skills_to_check) {
+            SEARCH_WARRIORS
+        } else {
+            SEARCH_WARRIORS_WITH_SKILLS
+        };
+        let warriors: Vec<Warrior> = sqlx::query_as(query)
             .bind(params.get("t"))
             .fetch_all(&state.primary_db_store)
             .await
@@ -38,6 +51,5 @@ pub async fn search_warriors(
         let _ = state.redis_store.set(&query_key, warriors_json).await;
 
         return (StatusCode::OK, headers, Json(warriors));
-    }    
-
+    }
 }
