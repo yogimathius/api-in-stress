ec7750ee78dde6f8e01a9d6ca2c413c2ede0b95e
Mathius <info@yogimathius.dev>
2024-04-29 23:24:56 -0700

    cleanup: rely on cache only for get_warrior

--- src/handlers/get_warrior.rs
+++ src/handlers/get_warrior.rs
@@ -9,15 +9,10 @@ use serde_json;
 
 use crate::models::Warrior;
 
-use crate::utilities::internal_error;
-
-use super::queries::GET_WARRIOR;
-
 pub async fn get_warrior(
     State(state): State<AppState>,
     Path(user_id): Path<String>,
 ) -> impl IntoResponse {
-    let user_id_clone = user_id.clone();
     let mut headers = HeaderMap::new();
     headers.insert("content-type", "application/json".parse().unwrap());
 
@@ -30,28 +25,12 @@ pub async fn get_warrior(
         let warrior: Warrior = serde_json::from_str(&redis_warrior).unwrap();
         return (StatusCode::OK, headers, Json(warrior));
     } else {
-        let result: Option<Warrior> = sqlx::query_as(&GET_WARRIOR)
-            .bind(user_id)
-            .fetch_optional(&state.primary_db_store)
-            .await
-            .map_err(|err| internal_error(err))
-            .unwrap();
-        match result {
-            Some(warrior) => {
-                let warrior_json: String = serde_json::to_string(&warrior).unwrap();
-                let _ = state.redis_store.set(&user_id_clone, warrior_json);
-
-                return (StatusCode::OK, headers, Json(warrior));
-            }
-            None => {
-                let no_warrior: Warrior = Warrior {
-                    id: "".to_string(),
-                    name: "".to_string(),
-                    dob: "".to_string(),
-                    fight_skills: None,
-                };
-                return (StatusCode::NOT_FOUND, headers, Json(no_warrior));
-            }
-        }
+        let no_warrior: Warrior = Warrior {
+            id: "".to_string(),
+            name: "".to_string(),
+            dob: "".to_string(),
+            fight_skills: None,
+        };
+        return (StatusCode::NOT_FOUND, headers, Json(no_warrior));
     }
 }
