294be0b66b9610eb805151e9abef35df63529101
Mathius <info@yogimathius.dev>
2024-05-01 00:00:13 -0700

    fix: optimize get queries

--- src/handlers/queries.rs
+++ src/handlers/queries.rs
@@ -1,21 +1,31 @@
 pub const GET_WARRIOR: &str = r#"
-SELECT warriors.*,
-    (SELECT array_agg(skills.name)
-        FROM skills
-        INNER JOIN warrior_skills ON skills.id = warrior_skills.skill_id
-        WHERE warrior_skills.warrior_id = warriors.id
-    ) AS fight_skills
-FROM warriors
-WHERE id = $1;"#;
+SELECT 
+    warriors.*, 
+    array_agg(skills.name) AS fight_skills
+FROM 
+    warriors
+LEFT JOIN 
+    warrior_skills ON warriors.id = warrior_skills.warrior_id
+LEFT JOIN 
+    skills ON warrior_skills.skill_id = skills.id
+WHERE 
+    warriors.id = $1
+GROUP BY 
+    warriors.id, warriors.name, warriors.dob;"#;
 
 pub const SEARCH_WARRIORS: &str = r#"
-SELECT warriors.*,
-    (SELECT array_agg(skills.name)
-        FROM skills
-        INNER JOIN warrior_skills ON skills.id = warrior_skills.skill_id
-        WHERE warrior_skills.warrior_id = warriors.id
-    ) AS fight_skills
-FROM warriors  
+SELECT 
+    warriors.*, 
+    array_agg(skills.name) AS fight_skills
+FROM 
+    warriors
+LEFT JOIN 
+    warrior_skills ON warriors.id = warrior_skills.warrior_id
+LEFT JOIN 
+    skills ON warrior_skills.skill_id = skills.id
 WHERE 
-    warriors.name ILIKE '%' || $1 || '%'
-LIMIT 50;"#;
+    warriors.name ILIKE $1
+GROUP BY 
+    warriors.id, warriors.name, warriors.dob
+LIMIT 
+    50;"#;
