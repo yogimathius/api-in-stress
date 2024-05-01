1ca7eed8156594b593a4d9db6516e0a3d23dfe63
Mathius <info@yogimathius.dev>
2024-05-01 00:01:12 -0700

    cleanup: dry app.rs

--- src/app.rs
+++ src/app.rs
@@ -5,10 +5,8 @@ use crate::handlers::{
     search_warriors::search_warriors,
 };
 use crate::redis::RedisDatabase;
-use crate::utilities::handle_timeout_error;
 use crate::valid_fight_skills::DbFightSkills;
 use axum::{
-    error_handling::HandleErrorLayer,
     http::Request,
     routing::{get, post},
     Router,
@@ -16,10 +14,8 @@ use axum::{
 use hyper::body::Incoming;
 use hyper_util::rt::{TokioExecutor, TokioIo};
 use hyper_util::server;
-use std::time::Duration;
 use tokio::net::TcpListener;
 use tower::Service;
-use tower::{timeout::TimeoutLayer, ServiceBuilder};
 
 pub struct Application {}
 
@@ -51,12 +47,6 @@ impl Application {
             .route("/warrior", get(search_warriors))
             .route("/counting-warriors", get(count_warriors))
             .with_state(app_state)
-            .layer(tower::ServiceBuilder::new().concurrency_limit(2000))
-            .layer(
-                ServiceBuilder::new()
-                    .layer(HandleErrorLayer::new(handle_timeout_error))
-                    .layer(TimeoutLayer::new(Duration::from_secs(30))),
-            )
     }
 
     pub async fn serve(listener: TcpListener, app: axum::Router) {
@@ -65,28 +55,14 @@ impl Application {
             let tower_service = app.clone();
             let (socket, _remote_addr) = listener.accept().await.unwrap();
             tokio::spawn(async move {
-                // Hyper has its own `AsyncRead` and `AsyncWrite` traits and doesn't use tokio.
-                // `TokioIo` converts between them.
                 let socket = TokioIo::new(socket);
 
-                // Hyper also has its own `Service` trait and doesn't use tower. We can use
-                // `hyper::service::service_fn` to create a hyper `Service` that calls our app through
-                // `tower::Service::call`.
                 let hyper_service =
                     hyper::service::service_fn(move |request: Request<Incoming>| {
-                        // We have to clone `tower_service` because hyper's `Service` uses `&self` whereas
-                        // tower's `Service` requires `&mut self`.
-                        //
-                        // We don't need to call `poll_ready` since `Router` is always ready.
                         tower_service.clone().call(request)
                     });
 
-                // `server::conn::auto::Builder` supports both http1 and http2.
-                //
-                // `TokioExecutor` tells hyper to use `tokio::spawn` to spawn tasks.
                 if let Err(err) = server::conn::auto::Builder::new(TokioExecutor::new())
-                    // `serve_connection_with_upgrades` is required for websockets. If you don't need
-                    // that you can use `serve_connection` instead.
                     .serve_connection_with_upgrades(socket, hyper_service)
                     .await
                 {
