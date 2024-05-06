5eec9a967c66643ac9677f62fb4944c088d96050
Mathius <info@yogimathius.dev>
2024-05-05 23:48:16 -0700

    refactor: pt 1 optimize get

--- src/handlers/get_warrior.rs
+++ src/handlers/get_warrior.rs
@@ -1,4 +1,4 @@
-use crate::app_state::AppState;
+use crate::{app_state::AppState, models::Warrior, utilities::internal_error};
 use axum::{
     extract::{Path, State},
     http::{HeaderMap, StatusCode},
@@ -7,51 +7,51 @@ use axum::{
 };
 use serde_json;
 
-use crate::models::Warrior;
-
-use crate::utilities::internal_error;
-
 use super::queries::GET_WARRIOR;
 
 pub async fn get_warrior(
     State(state): State<AppState>,
     Path(user_id): Path<String>,
 ) -> impl IntoResponse {
-    let user_id_clone = user_id.clone();
     let mut headers = HeaderMap::new();
     headers.insert("content-type", "application/json".parse().unwrap());
 
-    let redis_warrior = state
-        .redis_store
-        .get(&user_id.clone())
-        .await
-        .unwrap_or("".to_string());
-    if !redis_warrior.is_empty() {
+    if let Some(redis_warrior) = state.redis_store.get(&user_id).await {
         let warrior: Warrior = serde_json::from_str(&redis_warrior).unwrap();
         return (StatusCode::OK, headers, Json(warrior));
-    } else {
-        let result: Option<Warrior> = sqlx::query_as(&GET_WARRIOR)
-            .bind(user_id)
-            .fetch_optional(&state.primary_db_store)
-            .await
-            .map_err(|err| internal_error(err))
-            .unwrap();
-        match result {
-            Some(warrior) => {
-                let warrior_json: String = serde_json::to_string(&warrior).unwrap();
-                let _ = state.redis_store.set(&user_id_clone, warrior_json);
+    }
 
-                return (StatusCode::OK, headers, Json(warrior));
-            }
-            None => {
-                let no_warrior: Warrior = Warrior {
+    match sqlx::query_as(&GET_WARRIOR)
+        .bind(&user_id)
+        .fetch_optional(&state.primary_db_store)
+        .await
+    {
+        Ok(Some(warrior)) => {
+            let warrior_json: String = serde_json::to_string(&warrior).unwrap();
+            let _ = state.redis_store.set(&user_id, warrior_json).await;
+            (StatusCode::OK, headers, Json(warrior))
+        }
+        Ok(None) => {
+            let no_warrior = Warrior {
+                id: "".to_string(),
+                name: "".to_string(),
+                dob: "".to_string(),
+                fight_skills: None,
+            };
+            (StatusCode::NOT_FOUND, headers, Json(no_warrior))
+        }
+        Err(err) => {
+            let _ = internal_error(err);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                headers,
+                Json(Warrior {
                     id: "".to_string(),
                     name: "".to_string(),
                     dob: "".to_string(),
                     fight_skills: None,
-                };
-                return (StatusCode::NOT_FOUND, headers, Json(no_warrior));
-            }
+                }),
+            )
         }
     }
 }
